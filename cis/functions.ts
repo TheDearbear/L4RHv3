a23db339dc0e6f35ca5aa4471b1d6f27ccdf2ce1
Oleg <37769766+TheDearbear@users.noreply.github.com>
2024-08-30 00:30:25 +0300

    Implement rfind function

--- cis/functions.ts
+++ cis/functions.ts
@@ -6,7 +6,70 @@ export function rfind(
     backtrace: number[],
     args: Record<string | number, any> | null
 ): object[] | object | null {
-    return null;
+    if (args == null || typeof args.id !== 'number' || typeof args.max_backtrace !== 'number') {
+        throw new Error('Invalid args were passed');
+    }
+
+    var id = args.id;
+    var maxBacktrace = Math.min(args.max_backtrace, backtrace.length);
+
+    function getLayer(backtrace: number[]): DisassembledChunk[] {
+        let layer = global;
+        for (let i = 0; i < backtrace.length; i++) {
+            let layerIndex = backtrace[i];
+
+            if (layerIndex >= layer.length) {
+                throw new Error('Out-of-bounce backtrace index');
+            }
+
+            let chunk = layer[layerIndex];
+            if (!Array.isArray(chunk.data)) {
+                throw new Error('Chunk with data found');
+            }
+
+            layer = chunk.data;
+        }
+
+        return layer;
+    }
+
+    function rfindChunkById(source: DisassembledChunk, id: number): DisassembledChunk | null {
+        if (Array.isArray(source.data)) {
+            for (let i = source.data.length - 1; i >= 0; i--) {
+                let chunk = rfindChunkById(source.data[i], id);
+                if (chunk != null) {
+                    return chunk;
+                }
+            }
+        }
+
+        return source.id === id ? source : null;
+    }
+
+    function processLayer(backtrace: number[], iteration: number = 0): object[] | object | null {
+        let wasntEmptry = backtrace.length !== 0;
+        let limit = backtrace.pop() || global.length;
+
+        if (wasntEmptry && iteration === 0) {
+            limit--;
+        }
+        
+        let layer = getLayer(backtrace);
+        for (let i = limit; i >= 0; i--) {
+            let foundChunk = rfindChunkById(layer[i], id);
+            if (foundChunk != null) {
+                return foundChunk.data;
+            }
+        }
+
+        if (maxBacktrace < iteration) {
+            return processLayer(backtrace, iteration + 1);
+        }
+
+        return null;
+    }
+
+    return processLayer(backtrace);
 }
 
 export function find(current: object, global: object[], backtrace: number[], args: Record<string | number, any> | null): object | null {
